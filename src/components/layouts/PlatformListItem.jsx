49dfcb0498e89245e6df23465ec3f3dcef8a9ed9
Syed Shabbar <syedshabbar@Syeds-MacBook-Pro.local>
2025-12-07 02:08:54 +0500

    Force Vercel rebuild - rename import variable and clean up code

--- src/components/layouts/PlatformListItem.jsx
+++ src/components/layouts/PlatformListItem.jsx
@@ -1,14 +1,14 @@
-import Xicon from "../../assets/xIcon.png";
+import XiconImage from "../../assets/xIcon.png";
 
 // Platform list item component
 export default function PlatformListItem({ item, img }) {
   return (
-    <li className="flex gap-x-2  ">
-      {" "}
+    <li className="flex gap-x-2">
       <img
-        src={img ? img : Xicon}
+        src={img ? img : XiconImage}
         className="size-[1.05rem] mt-1 object-contain"
-      />{" "}
+        alt="icon"
+      />
       <span className="text-xs md:text-sm lg:text-lg">{item}</span>
     </li>
   );
